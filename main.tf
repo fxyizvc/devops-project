34ad58c05007f08e8f5769dd65a27130766c4914
Muhammed Fayiz V C <faisvc916@gmail.com>
2026-05-19 09:39:42 +0530

    Added Prometheus and Grafana monitoring stack via Docker Compose

--- main.tf
+++ main.tf
@@ -23,6 +23,22 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+# Allow Grafana (Dashboard)
+ingress {
+  from_port   = 3000
+  to_port     = 3000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# Allow Prometheus (Metrics Database)
+ingress {
+  from_port   = 9090
+  to_port     = 9090
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
   # Allow all outbound traffic so your server can download Docker
   egress {
     from_port   = 0
